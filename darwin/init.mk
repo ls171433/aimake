c4b46916c9ab1ea3b43abc6aeb1a5f0fe119a0c2
truthtracer <truthtracer@gmail.com>
2014-07-15 11:41:41 +0800

    add package in init.mk for all platform

--- darwin/init.mk
+++ darwin/init.mk
@@ -10,3 +10,23 @@ STRIP = strip
 CFLAGS   := -D __APPLE__ -D RUSAGE_THREAD=0 #-fPIC -pipe
 CXXFLAGS := $(CFLAGS)
 LDFLAGS := -lstdc++ 
+#
+# explict rules
+#
+%.o : %.c
+	$(CC) $(LOCAL_CFLAGS) $(CFLAGS) -c $< -o $@
+
+%.o : %.cc
+	$(CXX) $(LOCAL_CXXFLAGS) $(CXXFLAGS) -c $< -o $@
+
+%.o : %.cpp
+	$(CXX) $(LOCAL_CXXFLAGS) $(CXXFLAGS) -c $< -o $@
+
+
+#
+# building targets
+#
+EXECUTABLE = $(LOCAL_MODULE)
+SHARED_LIBRARY  = lib$(LOCAL_MODULE).so
+STATIC_LIBRARY  = lib$(LOCAL_MODULE).a
+PACKAGE  = $(LOCAL_MODULE)-$(TARGET_PLATFORM)-$(VERSION)-$(TIMESTAMP).tar.gz
