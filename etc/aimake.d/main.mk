474e100739fd6e6046938d349ffa647b4504e38e
ls171433 <lslslsls27@hotmail.com>
2016-02-22 14:48:37 +0800

    add default value for cc etc.

--- etc/aimake.d/main.mk
+++ etc/aimake.d/main.mk
@@ -2,6 +2,15 @@ BUILD_STATIC_LIBRARY = $(AIMAKE_HOME)/$(TARGET_PLATFORM)/build_static_library.mk
 BUILD_SHARED_LIBRARY = $(AIMAKE_HOME)/$(TARGET_PLATFORM)/build_shared_library.mk
 BUILD_EXECUTABLE     = $(AIMAKE_HOME)/$(TARGET_PLATFORM)/build_executable.mk
 
+CC    = cc
+LD    = ld
+CPP   = cpp
+CXX   = c++
+AR    = ar
+AS    = as
+NM    = nm
+STRIP = strip
+
 include $(AIMAKE_HOME)/$(TARGET_PLATFORM)/init.mk
 include $(AIMAKEFILE)
 include $(AIMAKE_HOME)/$(TARGET_PLATFORM)/build_all.mk
