90991e7338ad2fa4b823f533265e67563e0a1398
ls171433 <lslslsls27@hotmail.com>
2016-02-23 10:47:50 +0800

    change mingw init

--- etc/aimake.d/mingw/init.mk
+++ etc/aimake.d/mingw/init.mk
@@ -1,10 +1,7 @@
 TOOLCHAIN_DIR=/opt/bin
 TOOLCHAIN_PREFIX=i686-w64-mingw32-
 
-CC=gcc
-LD=ld
-
-MINGWABI=i686
+MINGWABI=$(shell uname -m)
 
 $(warning build for MINGWABI: $(MINGWABI))
 $(shell sleep 1)
@@ -14,41 +11,29 @@ ifeq ($(MINGWABI), i686)
     TOOLCHAINS = /opt/bin
     PREFIX=i686-w64-mingw32-
 
-    CC  = $(TOOLCHAINS)/$(PREFIX)gcc
-    LD  = $(TOOLCHAINS)/$(PREFIX)ld
-    CPP = $(TOOLCHAINS)/$(PREFIX)cpp
-    CXX = $(TOOLCHAINS)/$(PREFIX)g++
-    AR  = $(TOOLCHAINS)/$(PREFIX)ar
-    AS  = $(TOOLCHAINS)/$(PREFIX)as
-    NM  = $(TOOLCHAINS)/$(PREFIX)gcc-nm
-    STRIP = $(TOOLCHAINS)/$(PREFIX)strip
-    WINDRES = $(TOOLCHAINS)/$(PREFIX)windres
-
-    CFLAGS = -I $(TOOLCHAINS)/i686-w64-mingw32/include
-
 else ifeq ($(MINGWABI), x86_64)
 
-    TOOLCHAINS = /mingw64
-
-    CC  = $(TOOLCHAINS)/bin/gcc
-    LD  = $(TOOLCHAINS)/bin/ld  
-    CPP = $(TOOLCHAINS)/bin/cpp
-    CXX = $(TOOLCHAINS)/bin/g++
-    AR  = $(TOOLCHAINS)/bin/ar
-    AS  = $(TOOLCHAINS)/bin/as
-    NM  = $(TOOLCHAINS)/bin/nm
-    STRIP = $(TOOLCHAINS)/bin/strip
-    WINDRES = $(TOOLCHAINS)/bin/windres
-
-    CFLAGS = -I $(TOOLCHAINS)/x86_64-w64-mingw32/include
+    TOOLCHAINS = /opt/bin
+    PREFIX=x86_64-w64-mingw32-
 
 else
     $(error only support MINGWABI: i686, x86_64)
 endif
 
-CFLAGS   += -D __WIN32__ -DWIN32 #-fPIC -pipe
+CC    = $(TOOLCHAINS)/$(PREFIX)gcc
+LD    = $(TOOLCHAINS)/$(PREFIX)ld
+CPP   = $(TOOLCHAINS)/$(PREFIX)cpp
+CXX   = $(TOOLCHAINS)/$(PREFIX)g++
+AR    = $(TOOLCHAINS)/$(PREFIX)ar
+AS    = $(TOOLCHAINS)/$(PREFIX)as
+NM    = $(TOOLCHAINS)/$(PREFIX)gcc-nm
+STRIP = $(TOOLCHAINS)/$(PREFIX)strip
+
+WINDRES = $(TOOLCHAINS)/$(PREFIX)windres
+
+CFLAGS   += #-D __WIN32__ -DWIN32 #-fPIC -pipe
 CXXFLAGS += $(CFLAGS)
-LDFLAGS += -static-libgcc -static-libstdc++ 
+LDFLAGS  += -static-libgcc -static-libstdc++
 
 
 #
@@ -70,7 +55,7 @@ LDFLAGS += -static-libgcc -static-libstdc++
 #
 # building targets
 #
-EXECUTABLE = $(LOCAL_MODULE)
+EXECUTABLE = $(LOCAL_MODULE).exe
 SHARED_LIBRARY  = $(LOCAL_MODULE).dll
 STATIC_LIBRARY  = $(LOCAL_MODULE).lib
 PACKAGE  = $(shell basename .t/$(LOCAL_MODULE))-$(TARGET_PLATFORM)-$(VERSION)-$(TIMESTAMP).zip
